f6a93c36d123d855b235dd0452c3b8bb7d0184cd
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2025-04-09 19:44:29 +0200

    Basic project structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../UI/styles/global.css"
+
+export default function _app({ Component, pageProps }) {
+    return (
+        <>
+            <Component {...pageProps}/>
+        </>
+    )
+};
