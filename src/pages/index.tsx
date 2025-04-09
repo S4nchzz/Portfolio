d9d271202bdc9ecd0e21997f6705aeb763f4a34b
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2025-04-09 22:35:34 +0200

    Added TypeWritter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import styles from "../UI/styles/modules/index.module.css"
+import TypewriterComponent from "typewriter-effect";
+
 
 const Index = () => {
     return(
@@ -10,7 +12,25 @@ const Index = () => {
             />
 
             <section className={styles.presentation}>
-                <h1 className={styles.title}>Hi, my name is <span style={{color: '#74C9FF'}}>Iyan</span> and I'm a <span style={{color: '#74C9FF'}}>Full-Stack Developer</span>.</h1>
+                <h1 className={styles.title}>
+                    Hi, my name is
+                    <span style={{color: '#74C9FF'}}>
+                        <TypewriterComponent options={{
+                            strings: 'Iyan',
+                            autoStart: true,
+                            delay: 300,
+                            cursorClassName: styles.cursorColor
+                        }}/>
+                    </span>
+                    and I'm a
+                    <span style={{color: '#74C9FF'}}>
+                        <TypewriterComponent options={{
+                            strings: 'Full-Stack Developer.',
+                            autoStart: true,
+                            cursorClassName: styles.cursorColor
+                        }}/>
+                    </span>
+                    </h1>
                 <img
                 src="/img/home/profile_vector.png"
                 alt="Profile picture"
