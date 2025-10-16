7aa8e0bb120bee3ad120be8c1049e3c26374df3d
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2025-10-16 09:26:16 +0200

    ADD | Added searchbar(not functional) to the taskbar

--- src/components/ui/desktop_render/taskbar.tsx
+++ src/components/ui/desktop_render/taskbar.tsx
@@ -1,7 +1,10 @@
 import style from '@/styles/taskbar.module.css'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const Taskbar = () => {
+    const [searchBarFocused, setSearchBarFocused] = useState<boolean>(false)
+
     return (
         <div className={style.container}>
             <div className={style.wImageContainer}>
@@ -11,6 +14,26 @@ const Taskbar = () => {
                     alt='Wlogo'
                     width={32}
                     height={32}
+                />
+            </div>
+
+            <div
+                className={style.searchBar}
+                style={{
+                    backgroundColor: searchBarFocused ? '#2b2b2bff' : undefined
+                }}
+                onClick={() => setSearchBarFocused(!searchBarFocused)}>
+                <Image
+                    className={style.mgImage}
+                    src={'/img/desktop/mag_glass.svg'}
+                    alt='Wlogo'
+                    width={32}
+                    height={32}
+                />
+                
+                <input
+                    placeholder='Search'
+                    className={style.searchBarInput}
                 />
             </div>
         </div>
